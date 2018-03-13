142d4e164debd801071755122a63ffffd0ca72fc
Kate Groschner <ckgroschner@gmail.com>
2018-03-13 15:32:05 -0700

    added variable radii size

--- label_data_gui/learning_tkinter_addImg.py
+++ label_data_gui/learning_tkinter_addImg.py
@@ -12,12 +12,14 @@ from PIL import Image, ImageTk
 def init(data):
     data.circleCenters = [ ]
     data.recordCircleCenters = [ ]
+    data.radii = [ ]
 
 def mousePressed(event, data,canvas):
     newCircleCenter = (event.x, event.y)
     recordCircleCenter = (canvas.canvasx(event.x),canvas.canvasy(event.y))
     data.circleCenters.append(newCircleCenter)
-    data.circleCenters.append(recordCircleCenter)
+    data.recordCircleCenters.append(recordCircleCenter)
+    data.radii.append(data.radius)
 
 def keyPressed(event, data):
     if (event.char == "d"):
@@ -25,15 +27,22 @@ def keyPressed(event, data):
             data.circleCenters.pop(0)
         else:
             print("No more circles to delete!")
+    if event.keysym == "Up":
+        data.radius += 2
+        data.radii.pop()
+        data.radii.append(data.radius)
+    if event.keysym == "Down":
+        data.radius -= 2
+        data.radii.pop()
+        data.radii.append(data.radius)
 
 def redrawAll(canvas, data,photo):
     #draw the photo
-    canvas.create_image(512,512,image = photo)
+    canvas.create_image(0,0,image = photo,anchor = "nw")
     # draw the circles
-    for circleCenter in data.circleCenters:
+    for idx,circleCenter in enumerate(data.circleCenters):
         (cx, cy) = circleCenter
-        r = 20
-        canvas.create_oval(cx-r, cy-r, cx+r, cy+r, fill=None)
+        canvas.create_oval(cx-data.radii[idx], cy-data.radii[idx], cx+data.radii[idx], cy+data.radii[idx], fill=None, outline = "magenta")
     # draw the text
     canvas.create_text(data.width/2, 20,
                        text="Example: Adding and Deleting Shapes")
@@ -46,7 +55,7 @@ def redrawAll(canvas, data,photo):
 # use the run function as-is
 ####################################
 
-def run(width=1024, height=1024):
+def run(width=524, height=524):
     def redrawAllWrapper(canvas, data,photo):
         canvas.delete(ALL)
         canvas.create_rectangle(0, 0, data.width, data.height,
@@ -67,16 +76,19 @@ def run(width=1024, height=1024):
     data = Struct()
     data.width = width
     data.height = height
+    data.radius = 20
+    data
     root = Tk()
     init(data)
     image = Image.open('/Users/kategroschner/Box Sync/Research/HR-TEM/20180227/20180227_101729F_prepped20171204/8bit/20180227_101729F_plasma15sec_Mh370kx__0014.tif')
+    image = image.resize((512,512), Image.BICUBIC)
     photo = ImageTk.PhotoImage(image)
     #label = Label(image=photo)
     #label.image = photo
     #label.pack()
     # create the root and the canvas
-    canvas = Canvas(root, width=data.width, height=data.height)
-    canvas.create_image(512,512,image = photo)
+    canvas = Canvas(width=data.width, height=data.height)
+    canvas.create_image(0,0,image = photo, anchor = "nw")
     canvas.pack()
     # set up events
     root.bind("<Button-1>", lambda event:
@@ -87,6 +99,6 @@ def run(width=1024, height=1024):
     # and launch the app
     root.mainloop()  # blocks until window is closed
     print("bye!")
-    print(data.recordCircleCenters)
+    print(data.recordCircleCenters,data.radii)
 
-run(1024, 1200)
+run(524, 524)
