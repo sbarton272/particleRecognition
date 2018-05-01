008f2bccab737f19cd527c6bff7b250ae23e032a
ckgrosch <ckgroschner@gmail.com>
2018-04-30 18:55:48 -0700

    started CNN

--- classfier_builds/helperfuncs.py
+++ classfier_builds/helperfuncs.py
@@ -15,3 +15,24 @@ def check_label(labels,images,idx,alpha = 0.25):
     to show the overlay of the created label mask and original image"""
     pic = alpha*np.reshape(labels[idx][:,:,0],(1024,1024)) + (1-alpha)*np.reshape(images[idx][:,:,0],(1024,1024))
     imm(pic)
+
+def image_slice(directory, im_lbl):
+    """used to break up the 1024x1024 images into 512x512 segments"""
+    if im_lbl == 'image':
+        file_list = glob(directory+'/images/*.png')
+        new_directory = directory + '/sliced_images/'
+    elif im_lbl == 'label':
+        file_list = glob(directory+'/labels/*.png')
+        new_directory = directory + '/sliced_labels/'
+    name_list = [name.split('/')[-1].split('.')[0] for name in file_list]
+    for idx, file in enumerate(file_list):
+        image2split = plt.imread(file)
+        image1 = image2split[:512,:512,:]
+        plt.imsave(new_directory+name_list[idx]+'_a.png',image1, cmap='gray')
+        image1 = image2split[512:,:512,:]
+        plt.imsave(new_directory+name_list[idx]+'_b.png',image1, cmap='gray')
+        image1 = image2split[:512,512:,:]
+        plt.imsave(new_directory+name_list[idx]+'_c.png',image1, cmap='gray')
+        image1 = image2split[512:,512:,:]
+        plt.imsave(new_directory+name_list[idx]+'_d.png',image1, cmap='gray')
+    print('done!')
