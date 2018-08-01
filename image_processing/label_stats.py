de6890ec1fdf41679420454d5f69e17f401039a2
ckgrosch <ckgroschner@gmail.com>
2018-08-01 16:50:07 -0700

    updated particle stats

--- image_processing/label_stats.py
+++ image_processing/label_stats.py
@@ -17,14 +17,24 @@ def txt_reader(file):
         else:
             line = line.strip('\n')
             txt.append(line)
+    if 'Weird Data' == txt[0]:
+        weird = 'Not Weird Data'
+        start = 2
+    elif 'Weird Data' == txt[0]:
+        weird = 'Weird Data'
+        start = 2
+    else:
+        weird = 'No W Labeled'
+        start = 1
+    start = txt.index('Particle Location:') + 1
     center_stop = txt.index('Radius Size:')
     radius_stop = txt.index('Defect Label:')
     label_stop = txt.index('Image Size:')
-    for loc in txt[1:center_stop]:
+    for loc in txt[start:center_stop]:
         centers.append(literal_eval(loc))
     for loc in txt[center_stop+1:radius_stop] :
         radii.append(int(loc))
     for loc in txt[radius_stop+1:label_stop] :
         labels.append(loc)
     image_size = literal_eval(txt[-1])
-    return centers, radii, labels, image_size
+    return centers, radii, labels, weird
